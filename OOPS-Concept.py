cfef22f7f88c179d3ac120b4defaad5db8e2c0ce
Aamir Siddiqui <aamirsiddiqui196@gmail.com>
2020-04-17 15:50:02 +0530

    property final

--- OOPS-Concept.py
+++ OOPS-Concept.py
@@ -106,7 +106,53 @@ print(obj1.area())
 print(obj2.area())
 print(obj1 < obj2)
 
-# Property (Decorator)
+# Property (Decorator) start with _private variable to not let put -negative values later in widht or height
+class Rectangel:
+    def __init__(self, widht, height):
+        self._width = widht
+        self._height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, width):
+        if width < 0:
+            raise ValueError("Width must be positive")
+        else:
+            self._width = width
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, height):
+        if height < 0:
+            raise ValueError("Height must be positive")
+        else:
+            self._height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def __str__(self):
+        return "Rectangle: Width= {}, Height = {}".format(self.width, self.height)
+
+    def __repr__(self):
+        return "Rectangle({0}, {1})".format(self.width, self.height)
+
+    def __lt__(self, other):
+        if isinstance(other, Rectangel):
+            return self.area() < other.area()
+
+obj1 = Rectangel(20,40)
+obj1.width = -100
+
+# Property 2 : We don't use _width or _height private variable so now we can't put negative values in the beginning itsel
+# For ex r= Recatangel(-100,10) this will throw value error
+# In our above version we could have passed negative values initally i.e  r= (-100,10) but not later
 class Rectangel:
     def __init__(self, widht, height):
         self._width = widht
