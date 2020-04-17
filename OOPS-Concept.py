8a17eb11e462caff115b90468c1bf05d415339b1
Aamir Siddiqui <aamirsiddiqui196@gmail.com>
2020-04-17 12:55:27 +0530

    first version

--- OOPS-Concept.py
+++ OOPS-Concept.py
@@ -16,3 +16,25 @@ print(obj.height)
 # Change value of width
 obj.height=20
 print(obj.height)
+
+# __str__ and __repr__
+class Rectangel:
+    def __init__(self, widht, height):
+        self.width = widht
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        return "Rectangle: Width= {}, Height = {}".format(self.width, self.height)
+
+    def __repr__(self):
+        return "Rectangle({0}, {1})".format(self.width, self.height)
+
+obj = Rectangel(20,40)
+print(str(obj))
+print(obj)
