cba8f6c7d01ae96f3145e93652bc1ffbc314f6f2
Aamir Siddiqui <aamirsiddiqui196@gmail.com>
2020-04-17 15:44:28 +0530

    first version

--- OOPS-Concept.py
+++ OOPS-Concept.py
@@ -105,3 +105,47 @@ obj2= Rectangel(80,40)
 print(obj1.area())
 print(obj2.area())
 print(obj1 < obj2)
+
+# Property (Decorator)
+class Rectangel:
+    def __init__(self, widht, height):
+        self._width = widht
+        self._height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, width):
+        if width < 0:
+            raise ValueError("Width must be positive")
+        else:
+            self._width = width
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, height):
+        if height < 0:
+            raise ValueError("Height must be positive")
+        else:
+            self._height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def __str__(self):
+        return "Rectangle: Width= {}, Height = {}".format(self.width, self.height)
+
+    def __repr__(self):
+        return "Rectangle({0}, {1})".format(self.width, self.height)
+
+    def __lt__(self, other):
+        if isinstance(other, Rectangel):
+            return self.area() < other.area()
+
+obj1 = Rectangel(20,40)
+obj1.width = -100
